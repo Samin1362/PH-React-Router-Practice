f75f765463b769dde62a895127ccbe3d9d589887
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-10-07 08:36:51 +0600

    new book and books components added

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData } from "react-router";
+import Book from "../Book/Book";
 
 const Books = () => {
-
   const data = useLoaderData();
-  console.log(data);
 
   return (
     <div>
-      <h1 className='text-[40px] text-bold text-center'>Books</h1>
-
+      <h1 className="text-[40px] text-bold text-center">Books</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 justify-center items-center">
+        {data.map((book, idx) => (
+          <Book book={book} key={idx}></Book>
+        ))}
+      </div>
     </div>
   );
 };
